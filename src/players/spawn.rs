0681a69fdd67b91ebe1972d8661d0caf32b6610c
Bragin Anton <antonsayfermay@gmail.com>
2025-07-09 20:29:47 +0300

    rework spawn plugin

--- src/players/spawn.rs
+++ src/players/spawn.rs
@@ -1,52 +1,35 @@
 use bevy::prelude::*;
 
-use crate::WINDOW_SIZE;
 use crate::GameState;
+use crate::WINDOW_SIZE;
+
+use super::{Player, Left, Right};
 
 const X_OFFSET: f32 = 25.0;
 
-#[derive(Component, Debug)]
-enum Position {
-    Right,
-    Left
-}
-
-impl Position {
-    pub fn get_cords(&self) -> Vec3 {
-        match self {
-            Self::Right => vec3(WINDOW_SIZE.x / 2.0 - X_OFFSET, 0.0, 0.0),
-            Self::Left => vec3(WINDOW_SIZE.x / -2.0 + X_OFFSET, 0.0, 0.0)
-        }
-    }
-}
-
-#[derive(Component)]
-struct Player;
-
 fn spawn_players(mut commands: Commands) {
-    let right_cords = Position::Right.get_cords();
-    let left_cords = Position::Left.get_cords();
-
+    // Left player
     commands.spawn((
         Player,
-        Position::Left,
+        Left,
         Sprite {
             color: Color::WHITE,
             custom_size: Some(super::PLAYER_SIZE),
             ..default()
         },
-        Transform::from_xyz(left_cords.x, left_cords.y, left_cords.z)
+        Transform::from_xyz(-WINDOW_SIZE.x / 2.0 + X_OFFSET, 0.0, 0.0)
     ));
 
+    // Right player
     commands.spawn((
         Player,
-        Position::Right,
+        Right,
         Sprite {
             color: Color::WHITE,
             custom_size: Some(super::PLAYER_SIZE),
             ..default()
         },
-        Transform::from_xyz(right_cords.x, right_cords.y, right_cords.z)
+        Transform::from_xyz(WINDOW_SIZE.x / 2.0 - X_OFFSET, 0.0, 0.0)
     ));
 }
 
