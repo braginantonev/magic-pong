2993616075ca3923198bd20ee1b3e88624681a47
Bragin Anton <antonsayfermay@gmail.com>
2025-07-11 14:58:27 +0300

    add x_offset to spawn walls

--- src/walls/mod.rs
+++ src/walls/mod.rs
@@ -2,6 +2,12 @@ mod spawn;
 
 use bevy::prelude::*;
 
+use crate::WINDOW_SIZE;
+
+pub const X_OFFSET: f32 = 100.0;
+const HORIZONTAL_WALL_SIZE: Vec2 = vec2(WINDOW_SIZE.x - X_OFFSET * 2.0, 5.0);
+const VERTICAL_WALL_SIZE: Vec2 = vec2(5.0, WINDOW_SIZE.y);
+
 #[derive(Component)]
 struct Wall;
 
--- src/walls/spawn.rs
+++ src/walls/spawn.rs
@@ -3,10 +3,7 @@ use bevy_rapier2d::prelude::*;
 
 use crate::WINDOW_SIZE;
 
-use super::Wall;
-
-const HORIZONTAL_WALL_SIZE: Vec2 = vec2(WINDOW_SIZE.x, 5.0);
-const VERTICAL_WALL_SIZE: Vec2 = vec2(5.0, WINDOW_SIZE.y);
+use super::{Wall, X_OFFSET, HORIZONTAL_WALL_SIZE, VERTICAL_WALL_SIZE};
 
 fn spawn_walls(mut commands: Commands) {
     // Horizontal up wall
@@ -19,6 +16,8 @@ fn spawn_walls(mut commands: Commands) {
             custom_size: Some(HORIZONTAL_WALL_SIZE),
             ..default()
         },
+        Restitution::coefficient(1.0),
+        Friction::coefficient(0.0),
         Transform::from_xyz(0.0, WINDOW_SIZE.y / 2.0 - HORIZONTAL_WALL_SIZE.y, 0.0),
     ));
 
@@ -32,6 +31,8 @@ fn spawn_walls(mut commands: Commands) {
             custom_size: Some(HORIZONTAL_WALL_SIZE),
             ..default()
         },
+        Restitution::coefficient(1.0),
+        Friction::coefficient(0.0),
         Transform::from_xyz(0.0, -WINDOW_SIZE.y / 2.0 + HORIZONTAL_WALL_SIZE.y, 0.0),
     ));
 
@@ -45,7 +46,9 @@ fn spawn_walls(mut commands: Commands) {
             custom_size: Some(VERTICAL_WALL_SIZE),
             ..default()
         },
-        Transform::from_xyz(WINDOW_SIZE.x / 2.0 - VERTICAL_WALL_SIZE.x, 0.0, 0.0),
+        Restitution::coefficient(1.0),
+        Friction::coefficient(0.0),
+        Transform::from_xyz(WINDOW_SIZE.x / 2.0 - VERTICAL_WALL_SIZE.x - X_OFFSET, 0.0, 0.0),
     ));
 
     // Vertical left wall
@@ -58,7 +61,9 @@ fn spawn_walls(mut commands: Commands) {
             custom_size: Some(VERTICAL_WALL_SIZE),
             ..default()
         },
-        Transform::from_xyz(-WINDOW_SIZE.x / 2.0 + VERTICAL_WALL_SIZE.x, 0.0, 0.0),
+        Restitution::coefficient(1.0),
+        Friction::coefficient(0.0),
+        Transform::from_xyz(-WINDOW_SIZE.x / 2.0 + VERTICAL_WALL_SIZE.x + X_OFFSET, 0.0, 0.0),
     ));
 }
 
