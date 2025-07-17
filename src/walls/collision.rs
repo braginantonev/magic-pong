300c82a768b2954301dfbd5da21eacc1defb3055
Bragin Anton <antonsayfermay@gmail.com>
2025-07-18 00:06:37 +0300

    add next state pn ball collide

--- src/walls/collision.rs
+++ src/walls/collision.rs
@@ -9,12 +9,14 @@ use crate::{
     GameState
 };
 
+// При столкновении - увеличивает счёт игроку и переводит игру в состояние обновления очков
 fn ball_collision(
     mut score: ResMut<PlayersScore>,
     q_ball: Query<Entity, With<Ball>>,
     q_r_wall: Query<Entity, (With<Wall>, With<Right>)>,
     q_l_wall: Query<Entity, (With<Wall>, With<Left>)>,
-    mut collisions_events: EventReader<CollisionEvent>
+    mut collisions_events: EventReader<CollisionEvent>,
+    mut next_state: ResMut<NextState<GameState>>
 ) {
     let ball = q_ball.single().unwrap();
     let right_wall = q_r_wall.single().unwrap();
@@ -29,11 +31,13 @@ fn ball_collision(
 
                 if (*a == ball && *b == right_wall) || (*a == right_wall && *b == ball) {
                     score.add_point_to_right();
+                    next_state.set(GameState::UpdateScore);
                     println!("Point to the right, in all = {}", score.right_score());
                 }
 
                 if (*a == ball && *b == left_wall) || (*a == left_wall && *b == ball) {
                     score.add_point_to_left();
+                    next_state.set(GameState::UpdateScore);
                     println!("Point to the left, in all = {}", score.left_score());
                 }
             }
