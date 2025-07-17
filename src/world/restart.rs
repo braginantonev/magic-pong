62b7490a552c0e27c5f48642dd95ec0748256415
Bragin Anton <antonsayfermay@gmail.com>
2025-07-18 01:12:07 +0300

    add restart timer

---
+++ src/world/restart.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+
+use crate::GameState;
+
+#[derive(Resource)]
+struct RestartTimer(Timer);
+
+const RESTART_DURATION: f32 = 1.0;
+
+fn start_timer(mut timer: ResMut<RestartTimer>) {
+    timer.0.reset();
+}
+
+fn restart_game(
+    time: Res<Time>,
+    mut timer: ResMut<RestartTimer>,
+    mut next_state: ResMut<NextState<GameState>>
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        next_state.set(GameState::InGame);
+    }
+}
+
+pub struct RestartPlugin;
+
+impl Plugin for RestartPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .insert_resource(RestartTimer(Timer::from_seconds(RESTART_DURATION, TimerMode::Once)))
+            .add_systems(OnEnter(GameState::UpdateScore), start_timer)
+            .add_systems(Update, restart_game.run_if(in_state(GameState::Restart)));
+    }
+}
