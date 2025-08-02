c5047fd2af499335c77ec861c8f1b7b80f375d56
Bragin Anton <antonsayfermay@gmail.com>
2025-08-02 19:37:30 +0300

    [ui/p_b/placeholder] add skill animation Полное дерьмо требующее переработки, количество костылей повысилось в разы

--- src/ui/player_boards/placeholder/update.rs
+++ src/ui/player_boards/placeholder/update.rs
@@ -1,35 +1,61 @@
-use bevy::{math::VectorSpace, prelude::*};
+use bevy::prelude::*;
 
 use crate::{
-    GameState,
     players::{ 
         abilities::{
-            UseAbilityEvent,
-            Ultimates,
-            Skills,
+            Skills, Ultimates, UseAbilityEvent
         },
         score::IncreaseScoreEvent,
         Player,
         ULTIMATE_STEPS
-    }
+    }, GameState
 };
 
 use super::{
-    Placeholder, PPos, PlaceholderType,
+    Placeholder, PPos, PlaceholderType, PlaceholderAnimation,
     ULTIMATE_PLACEHOLDER_SIZE, ULTIMATE_PLACEHOLDER_POSITION, ULTIMATE_PLACEHOLDER_MAX_Y,
-    SKILL_PLACEHOLDER_SIZE, SKILL_PLACEHOLDER_POSITION, SKILL_PLACEHOLDER_MAX_Y
+    SKILL_PLACEHOLDER_POSITION
 };
 
 const ULTIMATE_DURATION: f32 = 1.0;
 const ULTIMATE_STEP_SIZE: f32 = ULTIMATE_PLACEHOLDER_MAX_Y / ULTIMATE_PLACEHOLDER_SIZE.y / ULTIMATE_STEPS as f32;
 
-#[derive(Component)]
-struct PlaceholderAnimation {
-    target_scale: Vec3,
-    start_scale: Vec3,
-    target_position: Vec3,
-    start_position: Vec3,
-    timer: Timer
+fn clear_skill_placeholder(
+    mut commands: Commands,
+    q_placeholders: Query<(Entity, &Transform, &Placeholder), Without<PlaceholderAnimation>>,
+    mut use_skill_event: EventReader<UseAbilityEvent<Skills>>
+) {
+    if use_skill_event.is_empty() {
+        return
+    }
+
+    let mut placeholder_pos: PPos = PPos::Left;
+
+    for ev in use_skill_event.read() {
+        placeholder_pos = ev.get_pos();
+    }
+
+    for (entity, transform, placeholder) in q_placeholders {
+        if placeholder.typ != PlaceholderType::Skill {
+            continue;
+        }
+
+        if placeholder.position != placeholder_pos {
+            continue;
+        }
+
+        commands.entity(entity).insert(PlaceholderAnimation {
+            start_position: transform.translation,
+            start_scale: transform.scale,
+            target_scale: vec3(1.0, 1.0, 1.0),
+            target_position: match placeholder_pos {
+                PPos::Right => SKILL_PLACEHOLDER_POSITION,
+                PPos::Left => SKILL_PLACEHOLDER_POSITION.with_x(-SKILL_PLACEHOLDER_POSITION.x)
+            },
+            timer: Timer::from_seconds(1.0, TimerMode::Once),
+            reload_needed: true
+        });
+    }
 }
 
 fn clear_ultimate_placeholder(
@@ -44,7 +70,7 @@ fn clear_ultimate_placeholder(
     let mut placeholder_pos: PPos = PPos::Left;
 
     for ev in use_ultimate_event.read() {
-        placeholder_pos = ev.get_pos()
+        placeholder_pos = ev.get_pos();
     }
 
     for (entity, transform, placeholder) in q_placeholders {
@@ -61,7 +87,8 @@ fn clear_ultimate_placeholder(
             start_scale: transform.scale,
             target_scale: transform.scale.with_y(ULTIMATE_PLACEHOLDER_SIZE.y),
             target_position: transform.translation.with_y(ULTIMATE_PLACEHOLDER_POSITION.y),
-            timer: Timer::from_seconds(ULTIMATE_DURATION, TimerMode::Once)
+            timer: Timer::from_seconds(ULTIMATE_DURATION, TimerMode::Once),
+            reload_needed: false
         });
     }
 }
@@ -88,7 +115,7 @@ fn increase_ultimate_placeholder(
         }
 
         if placeholder.position != increase_placeholder_pos {
-            continue
+            continue;
         }
 
         for player in q_players {
@@ -105,18 +132,26 @@ fn increase_ultimate_placeholder(
                 start_scale: transform.scale,
                 target_scale: transform.scale.with_y(transform.scale.y + ULTIMATE_STEP_SIZE),
                 target_position: transform.translation.with_y(transform.translation.y + ULTIMATE_PLACEHOLDER_MAX_Y / ULTIMATE_STEPS as f32 / 2.0),
-                timer: Timer::from_seconds(ULTIMATE_DURATION, TimerMode::Once)
+                timer: Timer::from_seconds(ULTIMATE_DURATION, TimerMode::Once),
+                reload_needed: false
             });
         }
     }
 }
 
-fn animate_placeholders(
+
+// Это разделение на анимацию ультимейта и скила такое дерьмо
+// Хочу сломать себе пальцы и заглянуть к себе в мозг
+fn animate_ult_placeholders(
     mut commands: Commands,
     time: Res<Time>,
-    q_placeholders: Query<(Entity, &mut Transform, &mut PlaceholderAnimation), With<Placeholder>>,
+    q_placeholders: Query<(Entity, &mut Transform, &mut PlaceholderAnimation, &Placeholder)>,
 ) {
-    for (entity, mut transform, mut anim) in q_placeholders {
+    for (entity, mut transform, mut anim, placeholder) in q_placeholders {
+        if placeholder.typ != PlaceholderType::Ultimate {
+            continue;
+        }
+
         anim.timer.tick(time.delta());
 
         transform.scale = anim.start_scale.lerp(anim.target_scale, anim.timer.fraction());
@@ -128,13 +163,42 @@ fn animate_placeholders(
     }
 }
 
+fn animate_skill_placeholders(
+    mut commands: Commands,
+    time: Res<Time>,
+    q_placeholders: Query<(Entity, &mut Transform, &mut PlaceholderAnimation, &Placeholder)>,
+) {
+    for (entity, mut transform, mut anim, placeholder) in q_placeholders {
+        if placeholder.typ != PlaceholderType::Skill {
+            continue;
+        }
+
+        anim.timer.tick(time.delta());
+
+        transform.scale = anim.start_scale.lerp(anim.target_scale, anim.timer.fraction());
+        transform.translation = anim.start_position.lerp(anim.target_position, anim.timer.fraction());
+
+        if anim.timer.finished() {
+            
+            //* This is a crutch for skill placeholder
+            //Todo: need optimization
+            if anim.reload_needed {
+                *anim = PlaceholderAnimation::skill_default(placeholder.position);
+                continue;
+            }
+
+            commands.entity(entity).remove::<PlaceholderAnimation>();
+        }
+    }
+}
+
 pub struct PlaceholderUpdatePlugin;
 
 impl Plugin for PlaceholderUpdatePlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(GameState::Restart), increase_ultimate_placeholder)
-            .add_systems(Update, animate_placeholders.run_if(in_state(GameState::Restart)).after(increase_ultimate_placeholder))
-            .add_systems(Update, clear_ultimate_placeholder.run_if(in_state(GameState::InGame)));
+            .add_systems(Update, animate_ult_placeholders.run_if(in_state(GameState::Restart)).after(increase_ultimate_placeholder))
+            .add_systems(Update, (clear_ultimate_placeholder, clear_skill_placeholder, animate_skill_placeholders).run_if(in_state(GameState::InGame)));
     }
 }
