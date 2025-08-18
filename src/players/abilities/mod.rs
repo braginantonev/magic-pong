776a2c70c2b808134712df80cc42f6a588588de6
Bragin Anton <antonsayfermay@gmail.com>
2025-08-18 21:35:55 +0300

    [p/abilities] add stager witch stage_timer

--- src/players/abilities/mod.rs
+++ src/players/abilities/mod.rs
@@ -2,12 +2,15 @@ mod ultimate;
 mod ultimates_rz;
 mod skill;
 mod skills_rz;
+mod abilities_db;
 
 use bevy::prelude::*;
 
 use crate::GameState;
 
 use super::PPos;
+
+const MIN_STAGE_TIME: f64 = 0.1;
 
 //* -- Ability Plugin -- */
 
@@ -16,12 +19,14 @@ pub struct AbilityPlugin;
 impl Plugin for AbilityPlugin {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(OnEnter(GameState::SpawnMainEntities), spawn_stager)
+            .add_event::<StageEntered>()
+            .add_systems(Update, tick_stage.run_if(in_state(GameState::InGame)))
             .add_plugins((
+                abilities_db::AbilitiesDBPlugin,
                 ultimate::UltimatePlugin,
                 ultimates_rz::UltimatesRealizationPlugin,
                 skill::SkillPlugin,
-                skills_rz::SkillsRealizationPlugin
+                skills_rz::SkillsRealizationPlugin,
             )); 
     }
 }
@@ -46,66 +51,51 @@ impl<T: Ability + Copy> UseAbilityEvent<T> {
 
 //* -- Ability Stages -- */
 
-#[derive(Clone, Copy)]
-enum Stages {
-    First,
-    Second,
-    Third,
-    End
-}
-
-trait Stage {}
-
-struct First;
-impl Stage for First {}
-
-struct Second;
-impl Stage for Second {}
-
-struct Third;
-impl Stage for Third {}
-
-struct End;
-impl Stage for End {}
-
-struct StageInfo {
-    stage: Stages,
-    duration: Option<f32>
-}
-
-impl StageInfo {
-    fn end() -> Self {
-        StageInfo { stage: Stages::End, duration: None }
-    }
-}
-
-struct StageQuery(Vec<StageInfo>);
-
-impl StageQuery {
-    fn get_and_next(&mut self) -> StageInfo {
-        if self.0.is_empty() {
-            return StageInfo::end()
-        } 
-
-        self.0.remove(0)
-    }
-}
-
-#[derive(Component)]
-struct AbilityStager;
-
-#[derive(Component)]
-struct StageTimer {
-    timer: Timer,
-    stage_query: StageQuery 
-}
-
-#[derive(Component)]
-struct AbilityStage<A: Ability, S: Stage> {
+#[derive(Event)]
+pub struct StageEntered {
+    ability: AbilitiesList,
     player: PPos,
+}
 
-    _ability: A,
-    _stage: S
+#[derive(Component)]
+struct Stager;
+
+#[derive(Component)]
+struct StageTimer{
+    timer: Timer,
+    player: PPos,
+    ability: AbilitiesList,
+}
+
+impl StageTimer {
+    pub fn new(player_pos: PPos, ability: AbilitiesList) -> Self {
+        StageTimer { timer: Timer::from_seconds(0.0, TimerMode::Once), player: player_pos, ability: ability }
+    }
+}
+
+fn tick_stage(
+    mut ability_info: ResMut<abilities_db::AbilitiesInfo>,
+    time: Res<Time>,
+    mut commands: Commands,
+    mut stage_ev: EventWriter<StageEntered>,
+    q_stagers: Query<(Entity, &mut StageTimer), With<Stager>>,
+) {
+    for (entity, mut stage_timer) in q_stagers {
+        stage_timer.timer.tick(time.delta());
+
+        if stage_timer.timer.finished() {
+            println!("write event");
+            stage_ev.write(StageEntered { ability: stage_timer.ability, player: stage_timer.player });
+
+            if !ability_info.add_to_counter(stage_timer.ability) { // Not in the end stage
+                println!("update timer");
+                stage_timer.timer = Timer::from_seconds(ability_info.get_current_stage_time(stage_timer.ability), TimerMode::Once);
+            } else {
+                println!("delete stager with timer");
+                commands.entity(entity).despawn();
+            }
+        }
+    }
 }
 
 struct ObjectAnimation {
@@ -121,30 +111,6 @@ struct ObjectAnimation {
 #[derive(Component)]
 struct AbilityStageAnimator(Vec<ObjectAnimation>);
 
-//Todo: Доделать аниматор
-fn tick_stage(
-    mut commands: Commands,
-    time: Res<Time>,
-    //q_stage_timer: Query<&mut AbilityStageTimer>,
-) {
-    for mut stage_timer in q_stage_timer {
-        stage_timer.timer.tick(time.delta());
-
-        if stage_timer.timer.finished() {
-            
-        }
-    }
-}
-
-fn spawn_stager(
-    mut commands: Commands
-) {
-    commands.spawn((
-        AbilityStager,
-        AbilityStageAnimator(Vec::new())
-    ));
-}
-
 
 //* -- Abilities functional -- */
 
@@ -152,7 +118,7 @@ pub trait Ability {
     fn to_str(&self) -> String;
 }
 
-#[derive(Clone, Copy)]
+#[derive(Hash, Clone, Copy, PartialEq, Eq)]
 pub enum SkillsList {
     Revert,
 }
@@ -160,12 +126,12 @@ pub enum SkillsList {
 impl Ability for SkillsList {
     fn to_str(&self) -> String {
         match self {
-            SkillsList::Revert => "Revert".to_string(),
+            Self::Revert => "Revert".to_string(),
         }
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Hash, Clone, Copy, PartialEq, Eq)]
 pub enum UltimatesList {
     Debug1,
     Debug2,
@@ -178,6 +144,12 @@ impl Ability for UltimatesList {
             UltimatesList::Debug2 => "Debug 2".to_string(),
         }
     }
+}
+
+#[derive(Hash, Clone, Copy, PartialEq, Eq)]
+enum AbilitiesList {
+    Skill(SkillsList),
+    Ultimate(UltimatesList)
 }
 
 pub struct AbilityQueue<T: Ability> {
