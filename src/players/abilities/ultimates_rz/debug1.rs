a081a185d69b51960c9419919e35610c1b0f5161
Bragin Anton <antonsayfermay@gmail.com>
2025-08-18 21:38:53 +0300

    [p/a/ultimates_rz] add stages for debug1 ult

--- src/players/abilities/ultimates_rz/debug1.rs
+++ src/players/abilities/ultimates_rz/debug1.rs
@@ -1,23 +1,39 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use rand::Rng;
 
-use super::{ 
-    Ability, AbilityStage,
-    First
+use crate::{
+    ball::{
+        Ball, MAX_START_SPEED_X, MAX_START_SPEED_Y,
+        spawn::gen_random_velocity_coef,
+    }, 
+    players::abilities::{
+        Ability,
+        AbilitiesList,
+        StageEntered,
+        UltimatesList,
+        abilities_db::AbilitiesInfo,
+    },
+    GameState,
 };
-
-use crate::GameState;
 
 pub struct Debug1Plugin;
 
 impl Plugin for Debug1Plugin {
     fn build(&self, app: &mut App) {
         app
-            .add_event::<AbilityStage<Debug1, First>>()
-            .add_systems(Update, db1_first_stage.run_if(in_state(GameState::InGame)).run_if(on_event::<AbilityStage<Debug1, First>>));
+            .add_event::<Ult1sDebug1Event>()
+            .add_event::<Ult2sDebug1Event>()
+            .add_event::<Ult3sDebug1Event>()
+            .add_systems(Update, check_debug1_stages.run_if(in_state(GameState::InGame)).run_if(on_event::<StageEntered>))
+            .add_systems(Update, ul_db1_1s.run_if(in_state(GameState::InGame)).run_if(on_event::<Ult1sDebug1Event>))
+            .add_systems(Update, ul_db1_2s.run_if(in_state(GameState::InGame)).run_if(on_event::<Ult2sDebug1Event>))
+            .add_systems(Update, ul_db1_3s.run_if(in_state(GameState::InGame)).run_if(on_event::<Ult3sDebug1Event>))
+        ;
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct Debug1;
 
 impl Ability for Debug1 {
@@ -26,8 +42,67 @@ impl Ability for Debug1 {
     }
 }
 
-fn db1_first_stage(
-    asr_event: EventReader<AbilityStage<Debug1, First>>
+//* Revert stages events */
+
+#[derive(Event)]
+struct Ult1sDebug1Event;
+
+#[derive(Event)]
+struct Ult2sDebug1Event;
+
+#[derive(Event)]
+struct Ult3sDebug1Event;
+
+// Кто это читает, знайте, что этот код полное дерьмище, конкретно из-за этой части можно побить автора, то есть меня
+// Никогда так не делайте пожалуйста
+fn check_debug1_stages(
+    abilities_info: Res<AbilitiesInfo>,
+    mut ult_1s_ev_writer: EventWriter<Ult1sDebug1Event>,
+    mut ult_2s_ev_writer: EventWriter<Ult2sDebug1Event>,
+    mut ult_3s_ev_writer: EventWriter<Ult3sDebug1Event>,
+    mut stage_entered_ev: EventReader<StageEntered>,
 ) {
-    
+    for ev in stage_entered_ev.read() {
+        if ev.ability != AbilitiesList::Ultimate(UltimatesList::Debug1) {
+            return
+        }
+
+        println!("start ev(current ability counter = {})", abilities_info.get_stage_counter(ev.ability).get_current_stage());
+        match abilities_info.get_stage_counter(ev.ability).get_current_stage() {
+            0 => { ult_1s_ev_writer.write(Ult1sDebug1Event); },
+            1 => { ult_2s_ev_writer.write(Ult2sDebug1Event); },
+            2 => { ult_3s_ev_writer.write(Ult3sDebug1Event); },
+            _ => continue
+        };
+    }
+}
+
+fn ul_db1_1s(
+    mut q_ball: Query<(&mut Velocity, &mut Transform), With<Ball>>,
+) {
+    let (mut velocity, mut transform) = q_ball.single_mut().unwrap();
+    let mut rng = rand::rng();
+    velocity.linvel = Vec2::ZERO;
+    velocity.angvel = 25.0;
+
+    transform.translation = vec3(200.0 * rng.random_range(-1.0..=1.0), 100.0 * rng.random_range(-1.0..=1.0), 0.0);
+}
+
+fn ul_db1_2s(
+    mut q_ball: Query<(&mut Velocity, &mut Transform), With<Ball>>,
+) {
+    let (mut velocity, mut transform) = q_ball.single_mut().unwrap();
+    let mut rng = rand::rng();
+    velocity.angvel = -10.0;
+
+    transform.translation = vec3(200.0 * rng.random_range(-1.0..=1.0), 100.0 * rng.random_range(-1.0..=1.0), 0.0);
+}
+
+// End
+fn ul_db1_3s(
+    mut q_ball: Query<&mut Velocity, With<Ball>>,
+) {
+    let mut velocity = q_ball.single_mut().unwrap();
+    velocity.angvel = 0.0;
+    velocity.linvel = vec2(gen_random_velocity_coef() * MAX_START_SPEED_X, gen_random_velocity_coef() * MAX_START_SPEED_Y);
 }
